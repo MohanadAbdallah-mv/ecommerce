9fecf7b6d54335aa69f5606c0e8a828609aec9ef
MohanadAbdallah-mv <mohanad1967@yahoo.com>
2023-10-09 14:41:27 +0300

    List view for best seller "See more " done

---
+++ lib/views/product_list.dart
@@ -0,0 +1,137 @@
+import 'dart:developer';
+
+import 'package:ecommerece/models/user_model.dart';
+import 'package:ecommerece/new_architecture/controller/firestore_controller.dart';
+import 'package:ecommerece/widgets/SearchBar.dart';
+import 'package:ecommerece/widgets/product_card_horizontal.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import '../constants.dart';
+import '../models/product.dart';
+import '../widgets/Category_card.dart';
+import '../widgets/CustomText.dart';
+
+class ProductList extends StatefulWidget {
+  MyUser user;
+  String? title;
+  String category;
+
+  ProductList(
+      {super.key, required this.user, this.title = "", required this.category});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  late Future<List<Product>?> products;
+  Future<List<Product>?> MyFutureBestSeller(String category) async {
+    var myfuture =
+        await Provider.of<FireStoreController>(context, listen: false)
+            .getBestSeller(category);
+
+//todo : implement either left and put it down to show error if it is left,null loading and right is data
+
+    return myfuture.right;
+  }
+@override
+  void initState() {
+  products=MyFutureBestSeller(widget.category);
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Shoppie",
+              style: GoogleFonts.sarina(
+                  textStyle: TextStyle(
+                      color: AppTitleColor, fontWeight: FontWeight.w400)),
+            ),
+          ),
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+        ),
+        bottomNavigationBar:
+            BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "home",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile")
+        ]),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(mainAxisSize: MainAxisSize.min,
+              children: [
+                SearchBarfor(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    children: [
+                      widget.title == ""
+                          ? CustomText(
+                              text: widget.title!,
+                              color: AppTitleColor,
+                              fontWeight: FontWeight.w700,
+                              size: 28,
+                            )
+                          : CustomText(
+                              text: widget.title!,
+                              color: AppTitleColor,
+                              fontWeight: FontWeight.w700,
+                              size: 28,
+                            ),
+                      Container(
+                          margin: EdgeInsets.only(left: 8),
+                          height: 40,
+                          width: 108,
+                          child: Center(
+                              child: Text(
+                            widget.category,
+                            style: TextStyle(
+                                fontFamily: "Readex",
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )),
+                          decoration: BoxDecoration(
+                              color: primaryColor,
+                              borderRadius: BorderRadius.circular(30),
+                              border: Border.all(color: Colors.white)))
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 12),
+                  child: FutureBuilder(
+                      future:products ,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData ) {
+                          return ListView.builder(
+                            shrinkWrap: true,
+                              itemCount: snapshot.data!.length,
+                              scrollDirection: Axis.vertical,
+                              itemBuilder: (context, index) =>
+                                  ProductCardHorizontal(
+                                      index: index,
+                                      product: snapshot.data![index],
+                                      user: widget.user));
+                        } else {
+
+                          return CircularProgressIndicator();
+                        }
+                      }),
+                ),
+
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/widgets/product_card_horizontal.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../constants.dart';
+import '../models/product.dart';
+import '../models/user_model.dart';
+import '../views/product_detailed.dart';
+import 'CustomButton.dart';
+import 'CustomText.dart';
+
+class ProductCardHorizontal extends StatefulWidget {
+  int index;
+  Product product;
+  late bool isDiscount;
+  bool isLiked;
+  MyUser user;
+
+  //late bool isDiscount;
+  ProductCardHorizontal(
+      {super.key,
+      required this.index,
+      required this.product,
+      required this.user,
+      this.isDiscount = false,
+      this.isLiked = false});
+
+  @override
+  State<ProductCardHorizontal> createState() => _ProductCardHorizontalState();
+}
+
+class _ProductCardHorizontalState extends State<ProductCardHorizontal> {
+  @override
+  Widget build(BuildContext context) {
+    print(widget.product.sub_category);
+    widget.isDiscount = widget.product.discount_price! > 0 ? true : false;
+    return Container(
+      margin: EdgeInsets.only(top: 0),
+      height: 176,
+      width: double.maxFinite,
+      //color: Colors.red,
+      child: Row(
+        children: [
+          Container(
+            width: 150,
+            height: 172,
+            child: Stack(
+              children: [
+                Positioned(
+                    left: 44,
+                    top: 36,
+                    child: Container(
+                      width: 64,
+                      height: 100,
+                      color: Colors.white,
+                      child: Image(
+                        image: NetworkImage(widget.product.image!),
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+                Positioned(
+                    right: 8,
+                    top: 12,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          widget.isLiked = !widget.isLiked;
+                          //todo : create global is Liked for that user with product controller
+                        });
+                      },
+                      child: SvgPicture.asset(
+                        widget.isLiked
+                            ? "assets/svg/like button=liked.svg"
+                            : "assets/svg/like button=defult.svg",
+                        fit: BoxFit.values.last,
+                        width: 32,
+                        height: 32,
+                      ),
+                    )),
+                Positioned(
+                    bottom: 3,
+                    right: 1,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 28,
+                      width: 72,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CustomText(
+                            text: widget.product.stars.toString(),
+                            align: Alignment.center,
+                            color: primaryColor,
+                            size: 12,
+                            fontfamily: "ReadexPro",
+                            fontWeight: FontWeight.w400,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellowAccent,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 12, top: 20),
+            width: 240,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(
+                  height: 44,
+                  width: 180,
+                  text: widget.product.name!,
+                  textalign: TextAlign.left,
+                  // trim: widget.product.name!.length >= 20 ? true : false,
+                  color: AppTitleColor,
+                  fontfamily: "ReadexPro",
+                  fontWeight: FontWeight.w400,
+                  size: 16,
+                ),
+                CustomText(
+                  height: 24,
+                  text: widget.product.Author_name!,
+                  color: AuthorColor,
+                  textalign: TextAlign.left,
+                  size: 15,
+                ),
+                widget.isDiscount
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(
+                            text: "EGP ${widget.product.discount_price}",
+                            color: AppTitleColor,
+                            fontfamily: "ReadexPro",
+                            fontWeight: FontWeight.w400,
+                            size: 15,
+                          ),
+                          CustomText(
+                            text: "EGP ${widget.product.price}",
+                            color: TypingColor,
+                            fontfamily: "ReadexPro",
+                            fontWeight: FontWeight.w400,
+                            size: 12,
+                            linethrough: true,
+                          )
+                        ],
+                      )
+                    : Column(
+                        children: [
+                          CustomText(
+                            text: "EGP ${widget.product.price}",
+                            color: AppTitleColor,
+                            fontfamily: "ReadexPro",
+                            fontWeight: FontWeight.w400,
+                            size: 15,
+                          ),
+                        ],
+                      ),
+                Row(mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 192,
+                      height: 48,
+                      alignment: Alignment.bottomLeft,
+                      padding: EdgeInsets.only(left: 14, bottom: 4),
+                      child: ListView.builder(
+                        reverse: true,
+                        scrollDirection: Axis.vertical,
+                        itemCount:
+                            widget.product.sub_category!.length > 1 ? 2 : 1,
+                        itemBuilder: (context, index) => CustomText(
+                          align: Alignment.bottomLeft,
+                          height: 24,
+                          text: widget.product.sub_category![index].toString(),
+                          fontfamily: "ReadexPro",
+                          fontWeight: FontWeight.w400,
+                          color: primaryColor,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductDetailed(
+                                    product: widget.product, user: widget.user)));
+                      },
+                      child: SvgPicture.asset(
+                        "assets/svg/add_to_cart.svg",
+                        fit: BoxFit.values.last,
+                        width: 20,
+                        height: 20,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
